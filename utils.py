93473567c61c32a568c3287b000407f7980fdc1c
shahroz butt <buttshahroz192@gmail.com>
2024-11-27 15:14:00 +0500

    formatting

--- utils.py
+++ utils.py
@@ -6,7 +6,10 @@ from safetensors import safe_open
 from typing import Tuple
 import os
 
-def load_hf_model(model_path: str, device: str) -> Tuple[PaliGemmaForConditionalGeneration, AutoTokenizer]:
+
+def load_hf_model(
+    model_path: str, device: str
+) -> Tuple[PaliGemmaForConditionalGeneration, AutoTokenizer]:
     # Load the tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="right")
     assert tokenizer.padding_side == "right"
